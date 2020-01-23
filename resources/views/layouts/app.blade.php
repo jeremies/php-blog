05b4632a814fab9aa855c5c90cab511fc1524db9
Joaquim Monserrat Companys <qmonserrat@mailoo.org>
2020-01-23 23:24:06 +0100

    register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,19 @@
         <ul>
             <li><a href="/new">New article</a></li>
             <li><a href="/">Articles</a></li>
+            <li class="right"><a href="/auth/register">Sign up</a></li>
+            <li class="right"><a href="/login">Sign in</a></li>
         </ul>
     </div>
     <div class="content">
+    @if (count($errors) > 0)
+		<div class="alert">
+			<strong>Whoops! Something went wrong!</strong>
+			@foreach ($errors->all() as $error)
+				<p>{{ $error }}</p>
+			@endforeach
+		</div>
+	@endif
     @yield('content')
     </div>
 </body>
