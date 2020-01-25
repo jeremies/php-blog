0730bdfe71bc9b56f8759bd501dfc3f99ef15d9b
Joaquim Monserrat Companys <qmonserrat@mailoo.org>
2020-01-25 16:45:38 +0100

    project done

--- tests/Feature/NewPostTest.php
+++ tests/Feature/NewPostTest.php
@@ -4,15 +4,13 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\User;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class NewPostTest extends TestCase {
 
+    use RefreshDatabase;
+    
     public function testWrongParams() {
         $user = factory(User::class)
                 ->make(['email' => 'test@user.laravel']);
@@ -23,7 +21,22 @@ class NewPostTest extends TestCase {
                 ['title' => 'the title', 'content' => 'ojhkjhg']
         );
         $response->assertSessionHasErrors('content');
-        $response->assertSessionHas('_old_input');;
+        $response->assertSessionHas('_old_input');
+        ;
+    }
+
+    public function testNewPost() {
+        $postParams = [
+            'title' => 'the title',
+            'content' => 'In a place far far away.'
+        ];
+        $user = factory(User::class)
+                ->make(['email' => 'test@user.laravel']);
+        $user->save();
+        $this->be($user);
+        $response = $this->call('POST', '/new', $postParams);
+        $response->assertRedirect('http://localhost/new');
+        $this->assertDatabaseHas('posts', $postParams);
     }
 
 }
